89ad9f1006e1c91008f3814504450977fe5f2d86
Marwa Mubarak <marwamubarak6@gmail.com>
2022-03-19 06:18:58 +0200

    Update app_root.dart

--- lib/src/app_root.dart
+++ lib/src/app_root.dart
@@ -6,7 +6,7 @@ class AppRoot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: HomeView(),
     );
   }
